1c6a417d6bc873121af99d09f45864b16cb134ff
Aswin-Sadanand <142343135+Aswin-Sadanand@users.noreply.github.com>
2024-12-02 23:56:31 +0530

    Add files via upload

---
+++ SCRIPT/SP[OrderDetails].sql
@@ -0,0 +1,41 @@
+
+GO
+ALTER PROCEDURE [GetRecentOrdersByCustomer]    
+    @CustomerId NVARCHAR(50),    
+    @Email NVARCHAR(100)    
+AS    
+BEGIN    
+    IF NOT EXISTS (SELECT 1 FROM CUSTOMERS WHERE CUSTOMERID = @CustomerId AND EMAIL = @Email)    
+    BEGIN    
+           SELECT 
+            'Customer not found or invalid credentials' AS errorMessage,    
+            JSON_QUERY('[]') AS orderDetails    
+        FOR JSON PATH, WITHOUT_ARRAY_WRAPPER;    
+        RETURN;    
+    END    
+    SELECT 
+        '' AS errorMessage, 
+        
+    (SELECT     
+        C.FIRSTNAME AS 'customer.firstName',    
+        C.LASTNAME AS 'customer.lastName',      
+        O.ORDERID AS 'order.orderNumber',    
+        O.ORDERDATE AS 'order.orderDate',    
+        O.DELIVERYEXPECTED AS 'order.deliveryExpected',      
+        (SELECT     
+            CASE WHEN O.CONTAINSGIFT=1 THEN 'GIFT' ELSE P.PRODUCTNAME END AS 'product',    
+            OI.QUANTITY AS 'quantity',    
+            OI.PRICE AS 'priceEach'    
+         FROM ORDERITEMS OI    
+         INNER JOIN PRODUCTS P ON OI.PRODUCTID = P.PRODUCTID    
+         WHERE OI.ORDERID = O.ORDERID    
+         FOR JSON PATH) AS 'order.orderItems'    
+    FROM ORDERS O    
+    INNER JOIN CUSTOMERS C ON O.CUSTOMERID = C.CUSTOMERID    
+    WHERE O.CUSTOMERID = @CustomerId    
+      AND C.EMAIL = @Email    
+    FOR JSON PATH) AS orderDetails    
+    FOR JSON PATH, WITHOUT_ARRAY_WRAPPER; 
+END
+
+GO
---
+++ SCRIPT/table insert.sql
@@ -0,0 +1,46 @@
+INSERT INTO CUSTOMERS (CustomerId, Firstname, Lastname, Email, HouseNo, Street, Town, PostCode)
+VALUES 
+('C34454', 'Bob', 'Marshal', 'bob@aol.com', '1A', 'Uppingham Gate', 'Uppingham', 'LE15 9NY'),
+('R34788', 'Jack', 'Ross', 'jack@yahoo.com', '4B', 'Strabrokeway', 'Wortley', 'LS12 4NB'),
+('A99001', 'Chris', 'Gregory', 'chris@gmail.com', '7A', 'Marsh Lane', 'Leeds', 'LS9 7NE'),
+('X45001', 'Ken', 'Martin', 'ken@aol.com', '32B', 'Harehills', 'York', 'LE15 9NY');
+
+go
+INSERT INTO PRODUCTS (PRODUCTNAME, COLOUR, SIZE)
+VALUES 
+('Tennis Ball', 'Yellow', 'S'),
+('Tennis Net', 'White', 'XL'),
+('Tennis Racket', 'White', 'L'),
+('Tennis Gear', 'White', 'XL');
+GO
+GO
+
+GO
+INSERT INTO ORDERS (CUSTOMERID, ORDERDATE, DELIVERYEXPECTED, CONTAINSGIFT)
+VALUES 
+('R34788', '2023-05-03', '2023-05-10', 0),
+('C34454', '2023-06-07', '2023-06-15', 0),
+('A99001', '2023-07-10', '2023-07-25', 1),
+('R34788', '2023-09-20', '2023-09-30', 0),
+('R34788', '2023-10-28', '2023-11-21', 1);
+GO
+INSERT INTO ORDERITEMS (ORDERID, PRODUCTID, QUANTITY, PRICE)
+VALUES 
+(1, 5, 3, 45),
+(1, 5, 1, 120),
+(2, 6, 1, 150),
+(2, 7, 1, 150),
+(3, 8, 5, 65),
+(3, 5, 1, 120),
+(4, 6, 2, 30),
+(5, 7, 1, 75);
+GO
+go
+
+
+GO
+
+select * from customers
+select * from products
+select * from orders
+select * from ORDERITEMS
